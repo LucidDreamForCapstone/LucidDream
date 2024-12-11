0588116cf16ed4dbb83dc72c3557c986e8e401ea
Hojin Lee <winghove@gmail.com>
2024-12-11 19:43:10 +0900

    글리치 아이템 획득 수정

--- Assets/00PuzzleTest/Scripts/SwapItem.cs
+++ Assets/00PuzzleTest/Scripts/SwapItem.cs
@@ -1,18 +1,32 @@
 using UnityEngine;
 
-public class SwapItem : ItemBase
+public class SwapItem : MonoBehaviour, Interactable
 {
+    private bool _prevState = false;
+    public string GetInteractText()
+    {
+        return "글리치 아이템 'G'";
+    }
+
+    public bool IsInteractBlock()
+    {
+        return false;
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
+        bool isInteractable = InteractManager.Instance.CheckInteractable(this);
+        if (isInteractable)
         {
-            PlayerSwapManager.Instance.SwapFlag = true;
-            gameObject.SetActive(false);
-            SystemMessageManager.Instance.PushSystemMessage("글리치 아이템을 획득했습니다!", Color.green);
+            if(_prevState != isInteractable)
+                SystemMessageManager.Instance.PushSystemMessage(GetInteractText(),Color.blue);
+            if (Input.GetKeyDown(KeyCode.G))
+            { 
+                PlayerSwapManager.Instance.SwapFlag = true;
+                gameObject.SetActive(false);
+                SystemMessageManager.Instance.PushSystemMessage("글리치 아이템을 획득했습니다!", Color.green);
+            }
         }
-        else if (InteractManager.Instance.CheckInteractable(this))
-        {
-            SystemMessageManager.Instance.PushSystemMessage("글리치 아이템 획득 ('G')", Color.green);
-        }
+        _prevState = isInteractable;
     }
 }
