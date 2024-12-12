27b3bd5dd95a798b35e7b0a6db0e3d012f225a2f
sihun <yangsh5106@gmail.com>
2024-12-13 04:38:56 +0900

    update : Swap 보스 예외 조건 변경

--- Assets/02.Scripts/Player/PlayerSwapManager.cs
+++ Assets/02.Scripts/Player/PlayerSwapManager.cs
@@ -12,6 +12,7 @@ public class PlayerSwapManager : MonoBehaviour {
     [SerializeField] private Player player1; // ù ��° ĳ����
     [SerializeField] private Player_2 player2; // �� ��° ĳ����
     [SerializeField] GameObject _miniMapObj;
+    [SerializeField] GungeonGameManager _dungeonManager;
     [SerializeField] float _portalSearchRadius;
     private MonoBehaviour currentPlayer; // ���� ���� ���� ĳ����
     private int currentPlayerNum;
@@ -80,8 +81,7 @@ public class PlayerSwapManager : MonoBehaviour {
     }
 
     private bool IsFinalBossStage() {
-        GameObject bondrewd = GameObject.Find("BossBondrewd");
-        return bondrewd != null;
+        return _dungeonManager.Stage == 5;
     }
 
 
